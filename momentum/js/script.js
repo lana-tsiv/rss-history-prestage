63d39ae1b2aaf19fe5993a8d9e462a834a3c3d8e
Lana <tiushkievich00@mail.ru>
2022-08-10 20:54:16 +0300

    feat: add date

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,9 +1,21 @@
 const App = () => {
   const time = document.querySelector(".time");
+  const date = document.querySelector(".date");
+  const options = {
+    month: "long",
+    day: "numeric",
+    weekday: "long",
+  };
 
   setInterval(() => (time.textContent = new Date().toLocaleTimeString()), 1000);
+  setInterval(
+    () => (
+      (date.textContent = new Date().toLocaleDateString("en-EN", options)),
+      60000
+    )
+  );
 
-  console.log(time);
+  console.log(date);
 };
 
 document.addEventListener("DOMContentLoaded", App);
